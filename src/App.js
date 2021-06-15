e1c4491bfd2d54e41836c2b1e0fe99e701cf3a0f
stmeem <stmeem@yahoo.com>
2021-06-15 20:23:50 +0600

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
+import tailwindcsslogo from "./tailwind-css-logo.svg";
 
 function App() {
   return (
-    <div className="">
-      <header className="">
+    <div className="mx-5 text-center mt-20 mb-8">
+      <header className="h-24 flex justify-center lg:h-48">
         <img src={logo} className="" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className=""
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={tailwindcsslogo} className="" alt="logo" />
       </header>
+
+      <p className="p-6 mb-6 ">
+        <h1 className="text-4xl">Tailwind CSS + Create React App </h1>
+      </p>
+      <a
+        className="text-blue-500"
+        href="https://tailwindcss.com/docs/guides/create-react-app"
+        target="_blank"
+      >
+        Find More about installation of Tailwind CSS with Create React App
+      </a>
     </div>
   );
 }
